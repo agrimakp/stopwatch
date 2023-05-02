d289b8a5c4d164fe3e1fdd8992f2c25a0a336a51
agrimakp <qvistn111@gmail.com>
2023-05-02 16:07:08 +1000

    .

--- timer.js
+++ timer.js
@@ -1,27 +1,56 @@
 window.onload = () => {
-    let minutes = 0;
-    let seconds = 0;
+    // in centiseconds
+    let count = 0;
     let resetHistory = [];
 
-    renderTime = (minutes, seconds) => {
+    // input in centiseconds
+    formatTime = (input) => {
+        let output = { minutes: 0, seconds: 0, cs: input };
+
+        // convert seconds into minute and seconds (100cs = 1 second)
+        let seconds = Math.floor(input / 100);
+
+        output.minutes = Math.floor(seconds / 60);
+        output.seconds = seconds % 60;
+        output.cs = input % 100;
+
+        console.log(input, output, seconds)
+
+        // cs?
+
+        // add leading zero if number less than 2 digits
+        if (output.seconds <= 9) {
+            output.seconds = "0" + output.seconds;
+        }
+        if (output.minutes <= 9) {
+            output.minutes = "0" + output.minutes;
+        }
+        if (output.cs <= 9) {
+            output.cs = "0" + output.cs;
+        }
+
+        return output
+    }
+
+    renderTime = (cs) => {
         let secondDisplay = document.getElementById("seconds");
         let minutesDisplay = document.getElementById("minutes");
-        if (seconds > 9) {
-            secondDisplay.innerHTML = seconds;
-        } else {
-            secondDisplay.innerHTML = "0" + seconds;
-        }
-        if (minutes > 9) {
-            minutesDisplay.innerHTML = minutes;
-        } else {
-            minutesDisplay.innerHTML = "0" + minutes;
-        }
+        let centiSecondsDisplay = document.getElementById("centiseconds")
+
+        // logic to do convertions
+        const time = formatTime(cs)
+
+        // sets time in minutes, seconds, centiseconds.
+        secondDisplay.innerHTML = time.seconds;
+        minutesDisplay.innerHTML = time.minutes;
+        centiSecondsDisplay.innerHTML = time.cs;
     }
 
     renderHistory = (history) => {
         let list = document.getElementById("history");
         items = history.map(item => {
-            return `<li>${item.minutes}:${item.seconds}</li>`;
+            let time = formatTime(item);
+            return `<li>${time.minutes}:${time.seconds}</li>`;
         })
         list.innerHTML = items.join("\n")
     }
@@ -31,11 +60,13 @@ window.onload = () => {
     let btnStop = document.getElementById("btn-stop");
     let btnReset = document.getElementById("btn-reset");
 
+    // store id so as to use this id with clearInterval() to cancel the timer
     let interval
 
     btnStart.onclick = () => {
         clearInterval(interval)
-        interval = setInterval(timer, 1000);
+        // method continues calling the function until clearInterval() is called in the background
+        interval = setInterval(timer, 10);
     }
 
     btnStop.onclick = () => {
@@ -43,23 +74,18 @@ window.onload = () => {
     }
 
     btnReset.onclick = () => {
-        resetHistory.push({ minutes, seconds })
-        minutes = 0;
-        seconds = 0;
-        renderTime(minutes, seconds)
+        resetHistory.push(count)
+        count = 0;
+        renderTime(count)
         renderHistory(resetHistory)
     }
 
     history.innerHTML = resetHistory;
 
     function timer() {
-        seconds++;
-
-        if (seconds > 59) {
-            minutes++;
-            seconds = 0;
-        }
-
-        renderTime(minutes, seconds)
+        // centiseconds
+        count++;
+        // display min, sec, cs
+        renderTime(count)
     }
 }
