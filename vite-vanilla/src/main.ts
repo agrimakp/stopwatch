d706e8928920994db70d8282e35016472c8cf689
agrimakp <qvistn111@gmail.com>
2023-05-03 22:22:54 +1000

    deleted stopwatch 2

--- vite-vanilla/src/main.ts
+++ vite-vanilla/src/main.ts
@@ -31,35 +31,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
           </ul>
       </div>
   </div>
-  <div class="wrapper" id="counter2">
-      <h1>StopWatch 2</h1>
-
-      <p>
-          <span id="minutes">00</span>
-          <span>:</span>
-          <span id="seconds">00</span>
-          <span>:</span>
-          <span id="centiseconds">00</span>
-      </p>
-      <div class="time">
-          <span>Minutes</span>
-          <span>Seconds</span>
-          <span>Centiseconds</span>
-      </div>
-
-      <div class="action">
-          <button id="btn-start">Start</button>
-          <button id="btn-stop">Stop</button>
-          <button id="btn-reset">Reset</button>
-      </div>
-      <div class="history">
-          <h4>History</h4>
-          <ul id="history">
-          </ul>
-      </div>
-  </div>
+  
   </div>
 `
 
 setStopWatch(document.querySelector<HTMLButtonElement>('#counter1')!)
-setStopWatch(document.querySelector<HTMLButtonElement>('#counter2')!)
